d922b16e51857acc206232a0bdea6acfaa3daa66
b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf <77827828+b43NnUNF4vidFYFhpqaLWy2ANawtRbMtUXZY9Pf@users.noreply.github.com>
2024-07-18 23:04:10 +0200

    Fix unrelesed reference to App

--- src/main.c
+++ src/main.c
@@ -3,5 +3,9 @@
 #include "app.h"
 
 int main(int argc, char *argv[]) {
-  return g_application_run(G_APPLICATION(app_new()), argc, argv);
+  App *app = app_new();
+  int status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
 }
